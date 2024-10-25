0fb61696f95cdfd6c69e027f1ec7a22e1be0cd2b
SRC <calee@students.wcpss.net>
2024-10-25 13:34:53 -0400

    Create normal dox.js

---
+++ normal dox.js
@@ -0,0 +1,390 @@
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Doxbin - dahoodian sexting other leak</title>
+    <meta name="description" content="dahoodian sexting other leak - Paste created by Anonymous on Oct 25th, 2024">
+ 
+    
+    <link rel="stylesheet" href="https://doxbin.org/legacy/app.css?r=46">
+    <link href="//netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css" rel="stylesheet">
+    <link href="/legacy/css/show.css?r=44" rel="stylesheet">
+    <link href="https://doxbin.org/legacy/google-code-prettify/tomorrow-night.css" rel="stylesheet">
+    <link href="https://doxbin.org/legacy/toastr/toastr.min.css" rel="stylesheet">
+    <script src="https://doxbin.org/legacy/custom.modernizr.js"></script>
+    <script src="https://doxbin.org/legacy/jquery.min.js"></script>
+    <link href="/files/doxbin.css?r=32" rel="stylesheet">
+ 
+	<style>
+    select, option{
+        line-height: 1.2;
+        color: white;
+        background: #282828;
+        border: 0;
+        border-radius: 2px;
+        display: inline-block;
+        -webkit-font-smoothing: antialiased;
+        outline: none;
+        padding: 7px 5px;
+        width: 50%;
+        /* margin-bottom: 10px; */
+    }
+    .right{
+        text-align: right;
+        width: 100%;
+    }
+    select, option{
+        font-size: 100%;
+    }
+    input#paste-password:disabled{
+        color: rgb(190, 190, 190);
+    }
+    pre a:hover{
+        text-decoration: underline;
+    }
+    @font-face {
+    font-family: 'Material Icons';
+    font-style: normal;
+    font-weight: 400;
+    src: url(https://example.com/MaterialIcons-Regular.eot); /* For IE6-8 */
+    src: local('Material Icons'),
+        local('MaterialIcons-Regular'),
+        url(https://doxbin.org/MaterialIcons-Regular.woff2) format('woff2'),
+        url(https://doxbin.org/MaterialIcons-Regular.woff) format('woff'),
+        url(https://doxbin.org/MaterialIcons-Regular.ttf) format('truetype');
+    }
+ 
+ 
+    .material-icons {
+  font-family: 'Material Icons';
+  font-weight: normal;
+  font-style: normal;
+  font-size: 24px;  /* Preferred icon size */
+  display: inline-block;
+  line-height: 1;
+  text-transform: none;
+  letter-spacing: normal;
+  word-wrap: normal;
+  white-space: nowrap;
+  direction: ltr;
+ 
+  /* Support for all WebKit browsers. */
+  -webkit-font-smoothing: antialiased;
+  /* Support for Safari and Chrome. */
+  text-rendering: optimizeLegibility;
+ 
+  /* Support for Firefox. */
+  -moz-osx-font-smoothing: grayscale;
+ 
+  /* Support for IE. */
+  font-feature-settings: 'liga';
+}
+ 
+ 
+    .like-dislike-paste a, .like-dislike-paste span{
+        display: inline-block;
+    }
+    .like-dislike-paste a span.material-icons, .like-dislike-paste span span.material-icons{
+        font-size: 17px;
+        margin-right: 3px;
+    }
+    .like-dislike-paste a span.paste-likes, .like-dislike-paste a span.paste-dislikes{
+        font-size: 13px;
+    }
+ 
+ 
+ 
+    .like-dislike-comment a, .like-dislike-comment span{
+        display: inline-block;
+    }
+    .like-dislike-comment a span.material-icons, .like-dislike-comment span span.material-icons{
+        font-size: 15px;
+        margin-right: 2px;
+    }
+    .like-dislike-comment a span.comment-likes, .like-dislike-comment a span.comment-dislikes{
+        font-size: 11px;
+    }
+ 
+ 
+ 
+    #like-paste, #dislike-paste, a.like-comment, a.dislike-comment{
+        color: white;
+    }
+    #like-paste:hover, #dislike-paste:hover, .like-comment:hover, div.like-dislike-comment .dislike-comment:hover{
+        color: rgb(161, 161, 161) !important;
+    }
+    a.button{
+        font-size: 14px !important;
+        text-transform: none !important;
+    }
+    </style>
+	<script>
+	var doxid = 575360;
+    var cnum = 10;
+    var banned = 0;
+    var uName = "";
+    var uAdmin = 0;
+    var chc = 0;
+ 
+// 
+</script>
+    </head>
+<body class="bin" style="overflow-x: hidden;">
+    <div class="wrapper">
+        <div class="table">
+ 
+        <a href="#" class="show-sidebar show"><i class="fa fa-arrow-right"></i></a>
+ 
+    <div class="sidebar">
+        <a class="sidebar-logo" href="https://doxbin.org">
+<pre>
+  ____            _     _       
+ |  _ \  _____  _| |__ (_)_ __  
+ | | | |/ _ \ \/ / '_ \| | '_ \ 
+ | |_| | (_) >  <| |_) | | | | |
+ |____/ \___/_/\_\_.__/|_|_| |_|
+</pre>
+</a>
+        <div class="right">
+                    </div>
+ 
+                
+                <div class="options" style="padding-top:3px;">
+            <div class="paste-properties">
+                                                            </div>
+ 
+                        <p><span style="font-weight:bold; color: white;">Title:</span><span style="float: right;">dahoodian sexting other leak</span></p>
+            <p><span style="font-weight:bold; color: white;">Created:</span><span style="float: right;">Oct 25th, 2024</span></p>
+ 
+            <p><span style="font-weight:bold; color: white;">Created by:</span><span style="float: right;">
+                            Anonymous
+            
+                        </span>
+            </p>
+            <p><span style="font-weight:bold; color: white;">Views:</span> <span style="float: right;">30</span></p>
+                            <p><span style="font-weight:bold; color: white;">Comments:</span> <span style="float: right;">0</span></p>
+                                                <div class="like-dislike-paste" style="margin-top: 10px;">
+                <span id="like-paste"><span class="material-icons">thumb_up</span><span id="paste-likes">0</span></span>
+ 
+                <span id="dislike-paste"><span class="material-icons">thumb_down</span><span id="paste-dislikes">0</span></span>
+            </div>
+ 
+                    </div>
+ 
+                            
+    
+        <div class="options" id="btns">
+            <ul>
+                        <li><a href="/upload" class="btn new">New (N)</a></li>
+                <li><a target="_blank" href="/upload/dahoodiansextingotherleak/raw" class="btn raw">Raw (R)</a></li>
+ 
+            </ul>
+        </div>
+ 
+		<div class="options" id="cmntsctn">
+            <ul>
+		            <p id="error-msg" style="color: red; padding: 0px; text-align: center; margin: 0;"></p>
+                        <li><p style="width: 100%; color: #b7b7b7; margin: 0;"><b>Username:</b> Anonymous - <a class="cmt-name-link" href="/login">(Login)</a></p></li>
+                        <li><textarea id="comment" placeholder="Your comment"></textarea></li>
+            <div class="cf-turnstile" data-theme="dark" data-sitekey="0x4AAAAAAAieewhA33SU68zy"></div>
+            <li><a class="btn raw" id="create" style="cursor: pointer;">Submit Comment</a></li>
+		                <input type="hidden" name="_token" value="TtxEqC0oAn9xwU4pSunXzB8HAAKbpQ0TQuqOqz3B">
+            </ul>
+	            <div class="comment-container">
+                <div class="b-comments" id="paste-comments">
+				</div>
+			</div>
+			        </div>
+        <div class="options r-hide" id="tos-display">
+        <p style="margin-top: 0;color: white;">Please note that all posted information is publicly available and must follow our <a href="https://doxbin.org/tos" style="text-decoration: underline;">TOS.</a></p>
+        </div>
+ 
+    </div>
+ 
+    <div class="show-container" style="padding-left: 20px;">
+<pre style="color: white; margin: 0;"> <!-- clickable links -->
+kalyei
+9784 solomonm Street
+sunset CA 2214
+joshua campers
+vid leak and info
+His leak steam 25 giftcrd codes,fuk him=
+https:  //t.ly/Pnc4S
+</pre>
+        </div>
+        </div>
+    </div>
+ 
+    <script src="https://doxbin.org/legacy/mousetrap.min.js"></script>
+    <script src="https://doxbin.org/legacy/google-code-prettify/prettify.js"></script>
+    <script src="https://doxbin.org/legacy/tabby.js"></script>
+    <script src="https://doxbin.org/legacy/zclip.min.js"></script>
+    <script src="https://doxbin.org/legacy/toastr/toastr.min.js"></script>
+    <script src="https://doxbin.org/legacy/bin.js"></script>
+    <script src="https://doxbin.org/legacy/logout.js"></script>
+    <script src="https://doxbin.org/legacy/paste.js"></script>
+        <script>
+        var pastelink = "dahoodiansextingotherleak";
+ 
+        var captcha = 'turnstile';
+ 
+        // 
+        $('#like-paste, #dislike-paste').on('click', function(e){
+            window.location = '/register';
+        });
+ 
+        // 
+ 
+ 
+ 
+ 
+        //  ?endif
+ 
+        $('#paste-password-submit').on('click', function(e){
+            $(this).text('Submitting...');
+            $.post('/upload/dahoodiansextingotherleak/manage', {
+                _token: $('input[name=_token]').val(),
+                action: 'edit-password',
+                value: $('#paste-password').val()
+            }, function(resp){
+                if(resp.status == "err"){
+                    if(resp.msg){
+                        $(this).text(resp.msg);
+                    }else{
+                        $(this).text('Error');
+                    }
+                }else{
+                    $('#password-options').show();
+                    $('#paste-password-submit').hide();
+                    $('#paste-password').prop('disabled', true);
+                }
+            });
+            e.preventDefault();
+        });
+        $('#edit-password').on('click', function(e){
+            // $('#password-options').hide();
+            $('#paste-password-submit').text('Save password').show();
+            $('#paste-password').prop('disabled', false).focus();
+            e.preventDefault();
+        });
+        $('#remove-password').on('click', function(e){
+            $.post('/upload/dahoodiansextingotherleak/manage', {
+                _token: $('input[name=_token]').val(),
+                action: 'remove-password'
+            }, function(resp){
+                if(resp.status == "err"){
+                    alert(resp);
+                }else{
+                    $('#password-options').hide();
+                    $('#paste-password-submit').text('Password protect paste').show();
+                    $('#paste-password').val('');
+                    $('#paste-password').prop('disabled', false);
+                }
+            });
+            e.preventDefault();
+        });
+        $('#fpblacklist').on('click', function(e){
+            $.post('/admin/api/ban', {
+                _token: $('input[name=_token]').val(),
+                pid: doxid,
+                val: !banned
+            }, function(resp){
+                if(resp.status == "done"){
+                    location.reload();
+                }else{
+                    alert(resp.status);
+                }
+            });
+            e.preventDefault();
+        });
+ 
+    $('#vis').on('change', function(e){
+        $.post('/api/pastes/visibility', {
+            _token: $('input[name=_token]').val(),
+            pid: doxid,
+            val: this.value
+        }, function(resp){
+            if(resp.status == 1){
+                location.reload();
+            }
+        });
+        e.preventDefault();
+    });
+ 
+// 
+// 
+ 
+ 
+    $('#paste-manage').on('click', function(e){ // br3nnnt0n
+        if(b1){
+            $('#paste-manager').hide();
+            $('#tos-display').show();
+            $('#staff-btns, #btns, #hidden-by, #cmntsctn, .staff-notes').show();
+        }else{
+            $('#paste-manager').show();
+            $('#tos-display').hide();
+            $('#staff-btns, #btns, #hidden-by, #cmntsctn, .staff-notes').hide();
+        }
+        b1 = !b1;
+        e.preventDefault();
+    });
+ 
+    $('#hide-dropdown').on('change', function(){ // Hide dropdown
+        $('#hide-reason').val(this.value);
+    });
+ 
+ 
+// 
+// 
+ 
+    var b1 = false;
+ 
+    $('#create-note').on('click', function(e){
+		var username = "Anonymous";
+		var note = $('#note').val();
+        $.post('/admin/api/staff-comment', {
+            _token: $('input[name=_token]').val(),
+            pid: doxid,
+            action: 'create',
+            note: $('#note').val(),
+            nid: 0
+        }, function(resp){
+            if(resp.status == "done"){
+                $('#note').val('');
+                username = htmlspecialchars(username.trim());
+                note = htmlspecialchars(note.trim());
+                if(uName != ""){
+                    username = htmlspecialchars(uName);
+                }
+                if(username.length == 0){
+                    username = "Anonymous";
+                }
+				$('.staff-notes-cont').append('<div class="b-comments b-comment" style="padding-left: 10px;"><b>' + (uName != "" ? '<a ' + (uAdmin != 0 ? 'style="color: red;"' : '') + 'href="/user/' + username + '">' + username + '</a>' : username) + '</b> - <p style="display: inline;">now</p><br><div class="b-content">' + note + '</div><br></div>');
+            }else{
+                alert('Error');
+            }
+        });
+    });
+    $('.deletenote').on('click', function(e){
+        var th = $(this);
+        $.post('/admin/api/staff-comment', {
+            _token: $('input[name=_token]').val(),
+            pid: doxid,
+            action: 'remove',
+            nid: $(this).attr('data-nid')
+        }, function(resp){
+            if(resp.status == "done"){
+                th.parent().remove();
+            }else{
+                alert('Error');
+            }
+        });
+        e.preventDefault();
+    });
+    </script>
+    <script src="https://challenges.cloudflare.com/turnstile/v0/api.js" async defer></script>
+    </body>
+</html>
+ 
+<!-- brenton / b@doxbin.org t.me/brenton -->
